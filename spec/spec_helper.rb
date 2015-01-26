98910fec329e18c29e891de312bf106b5b55f776
Lee Jorgensen & David Zulaica <student@epicodus.com>
2015-01-26 11:37:14 -0800

    Add initial files.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require("rspec")
+require("pg")
+require("sinatra/activerecord")
+require("employee")
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Employee.all().each() do |employee|
+      employee.destroy()
+    end
+  end
+end
